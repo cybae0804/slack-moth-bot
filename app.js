6254cd96f9fae831de6562c38925360de62b7339
cybae0804 <cybae0804@gmail.com>
2020-06-25 22:39:53 -0700

    More failsafe logic, comments, linted

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const express = require('express');
 const ky = require('ky-universal');
 const bodyParser = require('body-parser');
@@ -12,12 +13,15 @@ const web = new WebClient(token);
 let channelUsersMap = {};
 let userNameMap = {};
 
+// Because this is an async function, it cannot return the response directly back to slack.
+// Therefore, it uses response_url to send the response text back.
 async function addUser(url, channel_id, user_id, alias) {
+  if (!user_id) return 'Please provide a user id.';
   let response = `User *${user_id}* is already in this channel.`;
   if (!channelUsersMap[channel_id]) channelUsersMap[channel_id] = [];
   if (!channelUsersMap[channel_id].includes(user_id)) {
     channelUsersMap[channel_id].push(user_id);
-    
+
     if (!userNameMap[user_id]) {
       let name = alias;
 
@@ -33,10 +37,12 @@ async function addUser(url, channel_id, user_id, alias) {
     }
   }
 
-  ky.post(url, { json: { "text": response, "response_type": "in_channel" } });
+  ky.post(url, { json: { text: response, response_type: 'in_channel' } });
+  return true;
 }
 
 function removeUser(channel_id, user_id) {
+  if (!user_id) return 'Please provide a user id.';
   if (!channelUsersMap[channel_id].includes(user_id)) return `User *${user_id}* does not exist in this channel map.`;
 
   channelUsersMap[channel_id] = channelUsersMap[channel_id].filter(user => user !== user_id);
@@ -48,37 +54,41 @@ function removeUser(channel_id, user_id) {
 
   if (noOccurrence) delete userNameMap[user_id];
 
-  return `Removed user *${user_id}* from this channel map.`
-
+  return `Removed user *${user_id}* from this channel map.`;
 }
 
 function setAlias(user_id, alias) {
+  if (!user_id) return 'Please provide a user id.';
   if (!alias) {
     delete userNameMap[user_id];
     return `Removed alias for *${user_id}*.`;
   }
 
   userNameMap[user_id] = alias;
-  return `User *${user_id}* alias set to *${alias}*.`; 
+  return `User *${user_id}* alias set to *${alias}*.`;
 }
 
-function listUsers() {
+function listUsers(currentChannel) {
   let text = '';
 
-  if (Object.values(channelUsersMap).reduce((acc, curr) => curr && curr.length ? true : acc, false)) {
+  if (Object.values(channelUsersMap).reduce((acc, curr) => (curr && curr.length ? true : acc), false)) {
     text += '*Channels*\n';
     text += '```';
     Object.entries(channelUsersMap).forEach(([channel_id, users]) => {
       if (users.length) {
-        text += `${channel_id}:\n`;
+        text += `${channel_id}:`;
+
+        if (currentChannel === channel_id) text += ' (HERE)';
+        text += '\n';
+
         users.forEach(user_id => {
           text += `  ${user_id}: ${userNameMap[user_id]}\n`;
-        })
+        });
       }
     });
     text += '```';
   } else {
-    text += `No channels have been set.`;
+    text += 'No channels have been set.';
   }
 
   if (Object.values(userNameMap).length) {
@@ -89,25 +99,26 @@ function listUsers() {
     });
     text += '```';
   } else {
-    text += `\nNo aliases have been set.`;
+    text += '\nNo aliases have been set.';
   }
 
   return text;
 }
 
 function pickRandomUser(channel_id, user_id) {
-  let chosen;
-
   if (channelUsersMap[channel_id]) {
+    // Makes sure the callee does not get picked.
     const filtered = channelUsersMap[channel_id].filter(user => user !== user_id);
+
+    if (!filtered.length) return 'You\'re the only person in this channel.';
     const chosenId = filtered[Date.now() % filtered.length];
-    chosen = userNameMap[chosenId];
+    return userNameMap[chosenId] || chosenId;
   }
 
-  return chosen;
+  return 'There are no users in this channel.';
 }
 
-function resetHard() { 
+function resetHard() {
   channelUsersMap = {};
   userNameMap = {};
 
@@ -116,22 +127,23 @@ function resetHard() {
 
 function resetChannel(channel_id) {
   if (channelUsersMap[channel_id]) {
+    // Becasue removeUser alters the original array, creates a temporary copy to iterate over without being affected.
     const copy = [...channelUsersMap[channel_id]];
     copy.forEach(user_id => removeUser(channel_id, user_id));
 
-    return "Successfully reset this channel.";
+    return 'Successfully reset this channel.';
   }
 
-  return "No entires in this channel.";
+  return 'There are no users in this channel.';
 }
 
 function help() {
-  const add = `- add ['me' | user_id] [alias]\n  Adds a user into the current channel. If no alias is provided, first name will be used as alias.`;
-  const remove = `- remove ['me' | user_id]\n  Removes a user from the current channel. If the user doesn't belong in any other channels, the alias is also removed.`;
-  const alias = `- alias ['me' | user_id] [alias]\n  Sets the alias for a user. This alias is shared across any other channels. If no alias is passed in, it removes the alias.`;
-  const ls = `- ls\n  Shows the channels/aliases mapping.`;
-  const reset = `- reset ['hard']\n  Resets the current channel's users. 'hard' keyword resets all channels and aliases.`;
-  return '```' + [add, remove, alias, ls, reset].join('\n') + '```';
+  const add = '- add [\'me\' | user_id] [alias]\n  Adds a user into the current channel. If no alias is provided, first name will be used as alias.';
+  const remove = '- remove [\'me\' | user_id]\n  Removes a user from the current channel. If the user doesn\'t belong in any other channels, the alias is also removed.';
+  const alias = '- alias [\'me\' | user_id] [alias]\n  Sets a user\'s alias. This alias is shared across all channels. If no alias is passed in, it removes the user\'s alias.';
+  const ls = '- ls\n  Shows the channels/aliases mapping.';
+  const reset = '- reset [\'hard\']\n  Resets the current channel\'s users. \'hard\' keyword resets all channels and aliases.';
+  return `\`\`\`${[add, remove, alias, ls, reset].join('\n\n')}\`\`\``;
 }
 
 function handler(body) {
@@ -140,7 +152,7 @@ function handler(body) {
 
   switch (command) {
     case 'add': {
-      if (channel_name === 'directmessage') return 'Direct Messages cannot be added.';
+      if (channel_name === 'directmessage') return 'Direct Messages cannot be used.';
       return addUser(
         response_url,
         channel_id,
@@ -164,7 +176,7 @@ function handler(body) {
     }
 
     case 'ls': {
-      return listUsers();
+      return listUsers(channel_id);
     }
 
     case 'help': {
@@ -187,8 +199,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
   res.send({
-    "response_type": "in_channel",
-    "text": handler(req.body) || 'Something went wrong...',
+    response_type: 'in_channel',
+    text: handler(req.body) || 'Something went wrong...',
   });
 });
 
